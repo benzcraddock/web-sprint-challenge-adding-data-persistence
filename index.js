ac54593223a0e528e1c13ee976db28aaef0b4cb0
Benjamin Craddock <benjamincraddock@benjamins-mbp.lan>
2022-02-18 17:06:41 -0500

    index js and server js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// start your server here
+require('dotenv').config();
+
+const server = require('./api/server');
+
+// Bring port number from `process.env` variable, falling back to `9000` if `process.env.PORT` is undefined
+const PORT = process.env.PORT || 9000;
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
